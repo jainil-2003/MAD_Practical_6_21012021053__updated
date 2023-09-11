191f0a55e3fe6dbbf4d46412ff9b7621bbbbcb50
student <student@gnu.ac.in>
2023-09-11 10:15:56 +0530

    completed

--- app/src/main/java/com/example/mad_practical_6_21012021053/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_6_21012021053/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.mad_practical_6_21012021053
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val playbutton = findViewById<FloatingActionButton>(R.id.playbtn)
+        val stopbutton = findViewById<FloatingActionButton>(R.id.stopbtn)
+        playbutton.setOnClickListener {
+            play()
+        }
+
+        stopbutton.setOnClickListener {
+            stop()
+        }
+    }
+    fun play(){
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply { startService(this) }
+    }
+    fun stop(){
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply { startService(this) }
     }
 }
